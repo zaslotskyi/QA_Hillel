72da6eb8243fdaaab6f42cec306dbe8065611022
ruslan.zaslotskyi <ruslan.zaslotskyi@uklon.com.ua>
2024-03-21 18:25:11 +0200

    lesson1

---
+++ lesson1.py
@@ -0,0 +1,12 @@
+#1
+value = 3
+Value = 4
+value_value = 5
+value_3 = 6
+_value = 7
+#2
+print(value>Value)
+print(value<Value)
+print(value_value == 0)
+print(value_3 == _value)
+print(value_3 != _value)
